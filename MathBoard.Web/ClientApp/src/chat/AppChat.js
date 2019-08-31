2577bea35c5b85f7456950cfbbbf5e7f8d26e120
Chris Linton <cjlinton83@gmail.com>
2019-08-31 12:04:04 -0500

    connected to Chatkit.

--- MathBoard.Web/ClientApp/src/chat/AppChat.js
+++ MathBoard.Web/ClientApp/src/chat/AppChat.js
@@ -1,10 +1,42 @@
 ﻿import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
 
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 
+import { instanceLocator, tokenUrl } from './config'
+
 class AppChat extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    componentDidMount() {
+        const chatManager = new ChatManager({
+            instanceLocator,
+            userId: 'Admin',
+            tokenProvider: new TokenProvider({
+                url: tokenUrl
+            })
+        })
+
+        chatManager.connect()
+            .then(currentUser => {
+                console.log(`${currentUser.name} successfully connected to Chatkit.`)
+
+                this.setState({
+                    currentUser
+                })
+            })
+            .catch(err => {
+                console.log(`Error connecting to chatkit: ${err}`)
+            })
+    }
 
     render() {
         return (
--- MathBoard.Web/ClientApp/src/chat/config.js
+++ MathBoard.Web/ClientApp/src/chat/config.js
@@ -1,4 +1,4 @@
-﻿const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/445d0956-21d1-4173-b4a8-ca5f22cbfffb/token'
-const instanceLocator = 'v1:us1:445d0956-21d1-4173-b4a8-ca5f22cbfffb'
+﻿const instanceLocator = 'v1:us1:445d0956-21d1-4173-b4a8-ca5f22cbfffb'
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/445d0956-21d1-4173-b4a8-ca5f22cbfffb/token'
 
-export { tokenUrl, instanceLocator }
+export { instanceLocator, tokenUrl }
